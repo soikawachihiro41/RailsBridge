d143ba40e86121f9076a10a17649130180f71bbf
soikawa <soikawa0108@gmail.com>
2024-02-11 01:14:48 +0900

    toppegeとpostとcomment

--- db/schema.rb
+++ db/schema.rb
@@ -31,13 +31,6 @@ ActiveRecord::Schema[7.1].define(version: 2024_02_10_130555) do
     t.index ["user_id"], name: "index_posts_on_user_id"
   end
 
-  create_table "users", force: :cascade do |t|
-    t.string "name"
-    t.integer "age"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
   add_foreign_key "comments", "posts"
   add_foreign_key "comments", "users"
   add_foreign_key "posts", "users"
