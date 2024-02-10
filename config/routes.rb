69f1add54bdfe55f8f1cd6d53d0eb1bdb30c91fb
soikawa <soikawa0108@gmail.com>
2024-02-10 22:57:15 +0900

    topページとpostとcomment

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   root "tops#index"
   get "up" => "rails/health#show", as: :rails_health_check
-
+  resources :posts
+    resources :comments, only: [:create, :destroy]
 end
